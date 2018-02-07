653014cada1f2364dad84a29f864c513be8097b8
RyanTaylor13 <36234235+RyanTaylor13@users.noreply.github.com>
2018-02-07 09:40:43 -0500

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+/*
+ * Ryan Taylor
+ * Feb 6, 2018
+ * U1_01Ryanwpf
+ * Hello World - My first project
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U1_01_Ryan.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lb1Output.Content = "and I like Pizza";
+
+
+        }
+    }
+}
